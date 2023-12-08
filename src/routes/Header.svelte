dcaa3f3df87b02aaf7b2991e4abb2af8eebbb5c3
Fabrice Dufils Siyapdje <dev.dafab@gmail.com>
2023-12-08 12:28:09 +0100

    fix header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -48,77 +48,60 @@
 <svelte:window bind:scrollY bind:innerWidth={screenSize} />
 
 <!-- col-span-12 flex flex-col lg:flex-row justify-center gap-20 main-section -->
-<header class="fixed w-full z-10 top-0 min-h-12" class:bg-gray-100={scrollY > 76}>
-	<div class="w-full lg:w-2/3 mx-auto flex h-full">
-		<div class="w-full px-4 lg:px-0 leading-normal h-full grid grid-cols-5 items-center">
-			<p class="col-span-3 cursor-pointer logo" href={homeUrl}>
-				<span class="font-bold">Fabrice Dufils Siyapdje</span>
-			</p>
+<header class="fixed w-full z-10 top-0 min-h-12 flex items-center" class:bg-gray-100={scrollY > 76}>
+	<div class="w-full leading-normal container mx-auto grid grid-cols-12">
+		<p class="col-span-6 cursor-pointer logo" href={homeUrl}>
+			<span class="font-bold">Fabrice Dufils Siyapdje</span>
+		</p>
 
-			<button
-				on:click|preventDefault|stopPropagation={toggleMenuClicked}
-				bind:this={navToggle}
-				class="mobile-nav-toggle group block lg:hidden z-50"
-				aria-controls="nav-item-list"
-				aria-expanded="false"
+		<button
+			on:click|preventDefault|stopPropagation={toggleMenuClicked}
+			bind:this={navToggle}
+			class="mobile-nav-toggle group block lg:hidden col-end-11 z-50"
+			aria-controls="nav-item-list"
+			aria-expanded="false"
+		>
+			<span class="sr-only">Menu</span>
+			<div
+				data-open={menuExpanded === true || undefined}
+				class="relative flex overflow-hidden items-center justify-center rounded-full w-[65px] h-[65px] transform transition-all ring-0 ring-gray-300 hover:ring-8 data-[open=true]:ring-4 ring-opacity-30 duration-200"
 			>
-				<span class="sr-only">Menu</span>
 				<div
 					data-open={menuExpanded === true || undefined}
-					class="relative flex overflow-hidden items-center justify-center rounded-full w-[65px] h-[65px] transform transition-all ring-0 ring-gray-300 hover:ring-8 data-[open=true]:ring-4 ring-opacity-30 duration-200"
+					class="flex flex-col justify-between w-[30px] h-[30px] transform transition-all duration-300 origin-center overflow-hidden data-[open=true]:translate-x-1.5"
 				>
 					<div
 						data-open={menuExpanded === true || undefined}
-						class="flex flex-col justify-between w-[30px] h-[30px] transform transition-all duration-300 origin-center overflow-hidden data-[open=true]:translate-x-1.5"
-					>
-						<!-- .hamburger-icon { -->
-						<!--   display: flex; -->
-						<!--   flex-direction: column; -->
-						<!--   justify-content: space-between; -->
-						<!--   height: 24px; -->
-						<!--   width: 30px; -->
-						<!--   cursor: pointer; -->
-						<!-- } -->
-						<!---->
-						<!-- .hamburger-icon span { -->
-						<!--   width: 100%; -->
-						<!--   height: 2px; -->
-						<!--   background-color: black; -->
-						<!--   transition: all 0.3 ease-in-out; -->
-						<!-- } -->
-						<div
-							data-open={menuExpanded === true || undefined}
-							class="bg-black h-[2px] w-7 transform transition-all duration-300 origin-left data-[open=true]:rotate-[42deg] data-[open=true]:w-2/3 delay-150"
-						/>
-						<div
-							data-open={menuExpanded === true || undefined}
-							class="bg-black h-[2px] w-7 rounded transform transition-all duration-300 data-[open=true]:translate-x-10"
-						/>
-						<div
-							data-open={menuExpanded === true || undefined}
-							class="bg-black h-[2px] w-7 transform transition-all duration-300 origin-left data-[open=true]:-rotate-[42deg] data-[open=true]:w-2/3 delay-150"
-						/>
-					</div>
+						class="bg-black h-[2px] w-7 transform transition-all duration-300 origin-left data-[open=true]:rotate-[42deg] data-[open=true]:w-2/3 delay-150"
+					/>
+					<div
+						data-open={menuExpanded === true || undefined}
+						class="bg-black h-[2px] w-7 rounded transform transition-all duration-300 data-[open=true]:translate-x-10"
+					/>
+					<div
+						data-open={menuExpanded === true || undefined}
+						class="bg-black h-[2px] w-7 transform transition-all duration-300 origin-left data-[open=true]:-rotate-[42deg] data-[open=true]:w-2/3 delay-150"
+					/>
 				</div>
-			</button>
-			<nav class="col-span-1 lg:col-span-2 lg:flex lg:items-center">
-				<ul
-					use:click_outside={handle_click_outside}
-					bind:this={navigationList}
-					data-visible="false"
-					id="nav-item-list"
-					class="uppercase flex flex-col lg:flex-row p-[--mobile-nav-item-padding] lg:p-0 inset-[--mobile-nav-item-inset] lg:inset-[--nav-item-inset] bg-[--mobile-nav-background] lg:bg-[transparent] backdrop-blur-xl lg:backdrop-blur-none fixed translate-x-full lg:translate-x-0 ease-out duration-300 transition-transform"
-				>
-					{#each navItems as { icon, text, href }}
-						<li>
-							<a {href} data-sveltekit-preload-data="off">
-								<span on:click={toggleMenuClicked} class="m-auto ml-1"> {text} </span>
-							</a>
-						</li>
-					{/each}
-				</ul>
-			</nav>
-		</div>
+			</div>
+		</button>
+		<nav class="col-span-6 justify-end lg:flex lg:items-center items-end lg:items-end">
+			<ul
+				use:click_outside={handle_click_outside}
+				bind:this={navigationList}
+				data-visible="false"
+				id="nav-item-list"
+				class="uppercase flex flex-col lg:flex-row p-[--mobile-nav-item-padding] lg:p-0 inset-[--mobile-nav-item-inset] lg:inset-[--nav-item-inset] bg-[--mobile-nav-background] lg:bg-[transparent] backdrop-blur-xl lg:backdrop-blur-none fixed translate-x-full lg:translate-x-0 ease-out duration-300 transition-transform"
+			>
+				{#each navItems as { icon, text, href }}
+					<li>
+						<a {href} data-sveltekit-preload-data="off">
+							<span on:click={toggleMenuClicked} class="m-auto ml-1"> {text} </span>
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</nav>
 	</div>
 </header>
 
